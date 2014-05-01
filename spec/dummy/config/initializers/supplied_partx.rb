1f31c1a5dbc62f6ed60e63d192e6c77706bccb84
jc <emclab2011@gmail.com>
2014-05-01 13:16:08 -0500

    JC: prepared engine for search/stat. added 2 methods in application controller. Also added project_id when calling payment_requestx. spec passes.

--- spec/dummy/config/initializers/supplied_partx.rb
+++ spec/dummy/config/initializers/supplied_partx.rb
@@ -5,6 +5,6 @@ SuppliedPartx.manufacturer_class = 'Manufacturerx::Manufacturer'
 SuppliedPartx.show_project_path = 'heavy_machinery_projectx.project_path(r.project_id)'
 SuppliedPartx.show_customer_path = 'kustomerx.customer_path(r.customer_id)'
 SuppliedPartx.show_supplier_path = 'supplierx.supplier_path(r.supplier_id)'
-SuppliedPartx.index_payment_request_path = "payment_requestx.payment_requests_path(:resource_id => r.id, :resource_string => params[:controller])"
-SuppliedPartx.index_payment_request_supplied_partx_path = "payment_requestx.payment_requests_path(:resource_id => r.id, :resource_string => params[:controller], :subaction => 'supplied_partx')"
+SuppliedPartx.index_payment_request_path = "payment_requestx.payment_requests_path(:resource_id => r.id, :resource_string => params[:controller], :project_id => r.project_id)"
+SuppliedPartx.index_payment_request_supplied_partx_path = "payment_requestx.payment_requests_path(:resource_id => r.id, :resource_string => params[:controller], :project_id => r.project_id, :subaction => 'supplied_partx')"
 SuppliedPartx.payment_request_resource = 'payment_requestx/payment_requests'
